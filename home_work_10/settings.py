96f33b8fbf625b8b9869646b2c85c924f7b7ba40
andriymyr <andriymyr@gmail.com>
2024-01-15 00:14:19 +0200

    fix postgres

--- home_work_10/settings.py
+++ home_work_10/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "home_work_10.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "quotes-postgres",
+        "USER": "postgres",
+        "PASSWORD": "pass",
+        "HOST": "172.17.0.2",
+        "PORT": "",
     }
 }
 
